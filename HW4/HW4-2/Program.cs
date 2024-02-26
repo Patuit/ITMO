db93ddfe3ff74d80dd87fb59ecdf2c2818c960c3
Patuit <piligrim0606@gmail.com>
2024-02-26 23:03:45 +0300

    Topic 4

---
+++ HW4/HW4-2/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace HW4_2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /// 2.
+        /// Осуществить ввод последовательности целых чисел и сравнить,
+        /// что больше, количество положительных или количество отрицательных.
+        /// Последовательность потенциально не ограничена, окончанием последовательности служит число 0.
+        int inputValue = 0;
+        int sumPositiveNum = 0;
+        int sumNegativeNum = 0;
+        do
+        {
+            Console.Write("Введите число: ");
+            inputValue = Convert.ToInt32(Console.ReadLine());
+            if (inputValue > 0)
+            {
+                sumPositiveNum += inputValue;
+            }
+            else {
+                sumNegativeNum += Math.Abs(inputValue);
+            }
+        } while (inputValue != 0);
+        Console.WriteLine($"{(sumPositiveNum > sumNegativeNum ? "Сумма положительных больше" : sumPositiveNum < sumNegativeNum ? "Сумма отрицательных больше" : "Суммы равны")}!\nСумма положительных = {sumPositiveNum}\nСумма отрицательных = {sumNegativeNum}");
+    }
+}
---
+++ HW4/HW4-3/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace HW4_3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /// 3.
+        /// Ввести положительные числа A, B, C.
+        /// На прямоугольнике размера A x B размещено максимально возможное количество квадратов со стороной C (без наложений).
+        /// Найти количество квадратов, размещенных на прямоугольнике. Операции умножения и деления не использовать.
+
+        Console.Write("Введите A: ");
+        int A = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите B: ");
+        int B = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите C: ");
+        int C = Convert.ToInt32(Console.ReadLine());
+
+        if (C == 0 || B == 0 || A == 0)
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("НЕ СМЕЙ обманывать систему, глупец! Ты ввёл НОЛЬ!!!");
+            return;
+        }
+
+        if (C > A || C > B)
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("Ни один квадрат не поместится в прямоугольник!");
+            return;
+        }
+
+        int countSquare = 0;
+        int countColumn = 0;
+        int countLine = 0;
+
+        while ((countLine + C) <= B)
+        {
+            countLine += C;
+            while ((countColumn + C) <= A)
+            {
+                countColumn += C;
+                countSquare++;
+            }
+            countColumn = 0; // Дошли до конца строки и сбросили счётчик, настало время начать сначала..
+        }
+
+        // Выпендрился и обошёл склонения :Р
+        // Великое каверканье
+        Console.WriteLine($"В прямоугольник размером {A}x{B} поместится квадратов со стороной {C} в количестве штук: {countSquare}.");
+    }
+}
