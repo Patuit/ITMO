7929f716d5c5c5987f664abe36ff5b0d9086cc73
Patuit <piligrim0606@gmail.com>
2024-03-10 12:22:59 +0300

    Topic 7

---
+++ HW7/HW7-1/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace HW7_1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /* 
+        Два треугольника заданы длинами своих сторон.
+        Определить, площадь какого из них больше (создать метод для вычисления  площади  треугольника по длинам его сторон).
+        Для решения задачи можно использовать формулу Герона.
+        */
+
+        int[,] arrayTriangle = { { 2, 3, 4 }, { 4, 4, 5 } };
+        double maxSquare = 0;
+        int numMaxSquare = 0;
+
+        for (int i = 0; i < arrayTriangle.GetLength(0); i++)
+        {
+            double thisSquare = CalcSquare(arrayTriangle[i, 0], arrayTriangle[i, 1], arrayTriangle[i, 2]);
+
+            if (thisSquare > maxSquare)
+            {
+                maxSquare = thisSquare;
+                numMaxSquare = i + 1;
+            }
+        }
+
+        Console.WriteLine($"Самая большая площадь у треугольника под номером {numMaxSquare}");
+    }
+
+    static double CalcSquare(double sideFirst, double sideSecond, double sideThird)
+    {
+        double triangleHalfP = (sideFirst + sideSecond + sideThird) / 2;
+        return Math.Sqrt(triangleHalfP * (triangleHalfP - sideFirst) * (triangleHalfP - sideSecond) * (triangleHalfP - sideThird));
+    }
+}
---
+++ HW7/HW7-2/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace HW7_2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /* 
+            Создать метод для вычисления объема и площади поверхности куба по длине его ребра.
+        */
+        Console.Write("Введите длину ребра куба: ");
+        double edgeLength = Convert.ToDouble(Console.ReadLine());
+        double squareCube;
+        double volumeSquare;
+
+        CalcCube(edgeLength, out squareCube, out volumeSquare);
+
+        Console.WriteLine($"\nПлощадь куба = {squareCube:.00}\nПериметр куба = {volumeSquare:.00}");
+    }
+
+    static void CalcCube(double edgeLength, out double squareCube, out double volumeSquare)
+    {
+        squareCube = Math.Pow(edgeLength, 2) * 6;
+        volumeSquare = Math.Pow(edgeLength, 3);
+    }
+}
