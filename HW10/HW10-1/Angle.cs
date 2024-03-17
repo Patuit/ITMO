23bfdb3094da6b1cd8cec22271091e696ec25def
Patuit <piligrim0606@gmail.com>
2024-03-17 11:55:15 +0300

    Topic10

---
+++ HW10/HW10-1/Angle.cs
@@ -0,0 +1,112 @@
+﻿namespace HW10_1;
+
+public class Angle
+{
+    private int _gradus;
+    private int _minutes;
+    private int _seconds;
+
+    public DateTime Day { get; set; }
+
+    public int Gradus
+    {
+        get
+        {
+            return _gradus;
+        }
+        set
+        {
+            if (value < 360 && value >= 0)
+            {
+                _gradus = value;
+            }
+            else
+            {
+                throw new Exception("Ошибка. Неверно задаётся градус угла.");
+            }
+        }
+    }
+    public int Minutes
+    {
+        get
+        {
+            return _minutes;
+        }
+        set
+        {
+            if (value < 60 && value >= 0)
+            {
+                _minutes = value;
+            }
+            else
+            {
+                throw new Exception("Ошибка. Неверно задаются минуты угла.");
+            }
+        }
+    }
+    public int Seconds
+    {
+        get
+        {
+            return _seconds;
+        }
+        set
+        {
+            if (value < 60 && value >= 0)
+            {
+                _seconds = value;
+            }
+            else
+            {
+                throw new Exception("Ошибка. Неверно задаются секунды угла.");
+            }
+        }
+    }
+
+    public Angle(int gradus, int minutes, int seconds, DateTime day)
+    {
+        Gradus = gradus;
+        Minutes = minutes;
+        Seconds = seconds;
+        Day = day;
+    }
+
+    public double ToRadians()
+    {
+        double rad = (Gradus * Math.PI / 180) + (Minutes * Math.PI / (180 * 60)) + (Seconds * Math.PI / (180 * 3600));
+        return rad;
+    }
+
+    public override string ToString()
+    {
+        string answer = "";
+        switch (Gradus)
+        {
+            case >270:
+                answer = "Старая Луна (Убывающий месяц)";
+                break;
+            case 270:
+                answer = "Последняя четверть";
+                break;
+            case >180:
+                answer = "Убывающая Луна";
+                break;
+            case 180:
+                answer = "Полнолуние";
+                break;
+            case >90:
+                answer = "Растущая Луна";
+                break;
+            case 90:
+                answer = "Первая четверть";
+                break;
+            case 0:
+                answer = "Новолуние";
+                break;
+            case >0:
+                answer = "Молодая Луна (Растущий месяц)";
+                break;
+        }
+        return answer;
+    }
+}
---
+++ HW10/HW10-1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Globalization;
+
+namespace HW10_1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        try
+        {
+            Console.WriteLine("* Дневник наблюдения за Луной *");
+
+            bool flag = true;
+
+            List<Angle> joutnalMoon = new List<Angle>();
+
+            do
+            {
+                #region Ввод значений
+
+                string? input;
+                DateTime day;
+                do
+                {
+                    Console.Write("Введите дату наблюдения в формате дд.ММ.гггг (день.месяц.год): ");
+                    input = Console.ReadLine();
+                }
+                while (!DateTime.TryParseExact(input, "dd.MM.yyyy", null, DateTimeStyles.None, out day));
+
+                Console.Write("Введите градусы: ");
+                int gradus = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Введите минуты: ");
+                int minutes = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Введите секунды: ");
+                int seconds = Convert.ToInt32(Console.ReadLine());
+
+                #endregion
+
+                joutnalMoon.Add(new Angle(gradus, minutes, seconds, day));
+
+                Console.WriteLine("Enter - продолжить, f - завершить ввод.");
+                if (Console.ReadLine() == "f")
+                {
+                    flag = false;
+                }
+            } while (flag);
+
+
+            Console.WriteLine("\nЖурнал наблюдений:\n");
+            foreach (var item in joutnalMoon)
+            {
+                Console.WriteLine($"Дата наблюдения: {item.Day:D}");
+                Console.WriteLine($"Угол в градусах: {item.Gradus}° {item.Minutes}′ {item.Seconds}″");
+                Console.WriteLine($"Угол в радианах: {item.ToRadians():F5}");
+                Console.WriteLine($"Фаза луны: {item}\n");
+            }
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
