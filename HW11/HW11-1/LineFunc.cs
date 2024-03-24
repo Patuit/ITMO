25f1fa5f64a3cd7277e46c5b3d4dd68de2eeeb2a
Patuit <piligrim0606@gmail.com>
2024-03-24 13:07:40 +0300

    Topic11

---
+++ HW11/HW11-1/LineFunc.cs
@@ -0,0 +1,18 @@
+﻿namespace HW11_1;
+
+public struct LineFunc
+{
+    double k;
+    double b;
+
+    public LineFunc(double k, double b)
+    {
+        this.k = k;
+        this.b = b;
+    }
+
+    public double Root()
+    {
+        return -b / k;
+    }
+}
---
+++ HW11/HW11-1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace HW11_1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Решение линейного уравнения 0=kx+b");
+        
+        Console.Write("Введите k: ");
+        double k = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введите b: ");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        LineFunc func = new LineFunc(k, b);
+
+        Console.WriteLine($"Ответ: x = {func.Root()}");
+    }
+}
