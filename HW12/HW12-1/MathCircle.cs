823ee7add1b045def5b4b29308d2e9b669e9a8ad
Patuit <piligrim0606@gmail.com>
2024-03-24 15:21:11 +0300

    Topic12

---
+++ HW12/HW12-1/MathCircle.cs
@@ -0,0 +1,38 @@
+﻿namespace HW12_1;
+
+public static class MathCircle
+{
+    /// <summary>
+    /// Вычисляет периметр круга по радиусу
+    /// </summary>
+    /// <param name="radius"></param>
+    /// <returns>Периметр круга в double</returns>
+    public static double GetCircumference(double radius)
+    {
+        return Math.PI * 2 * radius;
+    }
+
+    /// <summary>
+    /// Вычисляет площадь круга по радиусу
+    /// </summary>
+    /// <param name="radius"></param>
+    /// <returns>Площадь круга в double</returns>
+    public static double GetScuare(double radius)
+    {
+        return Math.PI * Math.Pow(radius, 2);
+    }
+
+    /// <summary>
+    /// Проверка принадлежности точки(x, y) к кругу с центром(x0, y0) и заданным радиусом
+    /// </summary>
+    /// <param name="radius">Радиус круга</param>
+    /// <param name="x0">Значение x центра круга</param>
+    /// <param name="y0">Значение y центра круга</param>
+    /// <param name="x">Значение x проверяемой точки</param>
+    /// <param name="y">Значение y проверяемой точки</param>
+    /// <returns>true - точка принадлежит кругу, false - не принадлежит</returns>
+    public static bool GetInlineCircle(double radius, double x0, double y0, double x, double y)
+    {
+        return Math.Pow(x0 - x, 2) + Math.Pow(y0 - y, 2) < radius;
+    }
+}
---
+++ HW12/HW12-1/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace HW12_1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Вычисление периметра, площади круга по радиусу.\nВведите радиус: ");
+        do
+        {
+            try
+            {
+                double r = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine($"P = {MathCircle.GetCircumference(r):F3}, S = {MathCircle.GetScuare(r):F3}");
+                break;
+            }
+            catch (System.Exception)
+            {
+                Console.WriteLine("Неккоректный ввод. Попробуйте ввести ещё раз:");
+            }
+        } while (true);
+
+        Console.WriteLine("Проверка принадлежности точки(x, y) к кругу с центром(x0, y0) и заданным радиусом.");
+
+        do
+        {
+            try
+            {
+                Console.WriteLine("Введите координату точки x:");
+                double x = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Введите координату точки y:");
+                double y = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Введите координату центра круга x0:");
+                double x0 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Введите координату центра круга y0:");
+                double y0 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Введите радиус круга:");
+                double r = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine(MathCircle.GetInlineCircle(r, x0, y0, x, y) ? $"Точка({x}, {y}) принадлежит кругу с центром ({x0}, {y0}) и радиусом {r}." : "");
+                break;
+            }
+            catch (System.Exception)
+            {
+                Console.WriteLine("Неккоректный ввод. Попробуйте ввести ещё раз:");
+            }
+        } while (true);
+
+    }
+}
