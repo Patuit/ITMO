2c6ba3d1b8a4d2a1a52be7c471a537384c3da9e4
Patuit <piligrim0606@gmail.com>
2024-04-14 13:08:51 +0300

    Topic17

---
+++ HW17/HW17-1/ChetBank.cs
@@ -0,0 +1,24 @@
+﻿namespace HW17_1;
+
+public class ChetBank<T>
+{
+    private T num;
+    private int balance;
+    private string name;
+
+    public void Input()
+    {
+        Console.WriteLine("Введите номер счёта:");
+        num = (T)Convert.ChangeType(Console.ReadLine(), typeof(T));
+        Console.WriteLine("Введите баланс счёта:");
+        balance = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите ФИО счёта:");
+        name = Console.ReadLine();
+    }
+
+    public void ReadInfo()
+    {
+        Console.WriteLine($"Номер счёта: {num}, баланс: {balance}, ФИО: {name}.");
+    }
+
+}
---
+++ HW17/HW17-1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace HW17_1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ChetBank<string> chet1 = new();
+        chet1.Input();
+        chet1.ReadInfo();
+        ChetBank<int> chet2 = new();
+        chet2.Input();
+        chet2.ReadInfo();
+    }
+}
